f3a27383df99c186d44d6ad5738e6a54562fff75
Rubén Díaz Martínez <rubendm92@gmail.com>
2026-02-01 10:41:01 +0000

    Shuffle words

--- frontend/src/services/wordService.ts
+++ frontend/src/services/wordService.ts
@@ -18,6 +18,15 @@ const words: Word[] = [
   { word: 'Stuhl', article: 'der' },
 ];
 
+function shuffle<T>(array: T[]): T[] {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
 export async function getWords(): Promise<Word[]> {
-  return words;
+  return shuffle(words);
 }
