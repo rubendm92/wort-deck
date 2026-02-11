4b9656cc695e9c294aa142a94d1f9727ced16d86
Rubén Díaz Martínez <rubendm92@gmail.com>
2026-02-11 17:57:25 +0000

    Fix endpoint issue

--- api/words.ts
+++ api/words.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { words } from './data/words';
+import { words } from './data/words.js';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
