9ad7f64678b1a90c50587e74173ed960bc54b640
Rubén Díaz Martínez <rubendm92@gmail.com>
2026-02-11 18:55:41 +0000

    Fix function

--- frontend/src/hooks/useDebounce.ts
+++ frontend/src/hooks/useDebounce.ts
@@ -1,10 +1,11 @@
 import { useEffect, useRef } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useDebounce<T extends (...args: any[]) => void>(
   callback: T,
   delay: number
 ): T {
-  const timeoutRef = useRef<number>();
+  const timeoutRef = useRef<number | undefined>(undefined);
   const callbackRef = useRef(callback);
 
   // Update callback ref when it changes
@@ -21,6 +22,7 @@ export function useDebounce<T extends (...args: any[]) => void>(
     };
   }, []);
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return ((...args: any[]) => {
     if (timeoutRef.current) {
       clearTimeout(timeoutRef.current);
